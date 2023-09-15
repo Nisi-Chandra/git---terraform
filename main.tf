cb8a54a013ee083790e6c2f7f71ccc83bd40e558
nisi <nisi@gmail.com>
2023-09-15 13:54:43 +0000

    wordpress -sg-bastion created

--- main.tf
+++ main.tf
@@ -154,3 +154,35 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_security_group" "bastion" {
+  name_prefix = "${var.project_name}-${var.project_environment}-bastion-"
+  description = "bastion security group"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_environment}-bastion"
+    project = var.project_name
+    Env     = var.project_environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
