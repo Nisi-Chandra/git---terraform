bfbb998b31c20b17028a6a8f45aab0cdd8fbdd63
nisi <nisi@gmail.com>
2023-09-15 14:01:41 +0000

    Front-end-security group created

--- main.tf
+++ main.tf
@@ -186,3 +186,50 @@ resource "aws_security_group" "bastion" {
   }
 }
 
+###########Front-end-security group####
+
+
+resource "aws_security_group" "frontend" {
+  name_prefix = "${var.project_name}-${var.project_environment}-frontend-"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_environment}-frontend-"
+    project = var.project_name
+    Env     = var.project_environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
