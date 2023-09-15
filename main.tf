9f935eb7327746275677469d7800f3f344a2b7ad
nisi <nisi@gmail.com>
2023-09-15 13:09:07 +0000

    public subnets created

--- main.tf
+++ main.tf
@@ -27,3 +27,20 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Subnet creation -public
+
+
+resource "aws_subnet" "public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 3, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name    = "${var.project_name}-${var.project_environment}-public${count.index + 1}"
+    project = var.project_name
+    Env     = var.project_environment
+  }
+}
+
+
