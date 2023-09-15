19cf8b39454c88f9bda13053edeea9d1abf1b8b0
nisi <nisi@gmail.com>
2023-09-15 13:45:23 +0000

    Public and private route table association created

--- main.tf
+++ main.tf
@@ -133,4 +133,24 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Route table association - Public
+
+
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Route table association - Private
+
+
+
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
 
